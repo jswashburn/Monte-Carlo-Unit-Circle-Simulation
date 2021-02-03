dbb89e9c4d4d39822c5853e8b8012a4d6ef483f8
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-02-02 23:03:14 -0500

    Readability, conventions

--- CoordinatePair.cs
+++ CoordinatePair.cs
@@ -1,24 +1,20 @@
-﻿using System;
+using System;
 
 namespace MonteCarloUnitCircleSimulation
 {
+    // Represents a first-quadrant co-ordinate pair.
     class CoordinatePair
-    {
-        // Represents a top-right quadrant co-ordinate pair (positive X and Y).
-
-        float X { get; }
-        float Y { get; }
-        float Hypotenuse => (float)Math.Sqrt((X * X) + (Y * Y));
+    {  
+        float _x, _y;
+        float _hypotenuse => (float)Math.Sqrt((_x * _x) + (_y * _y));
 
         public CoordinatePair(float x, float y)
         {
-            X = x;
-            Y = y;
+            _x = x;
+            _y = y;
         }
 
         // The pair overlaps the inner area of a circle if its hypotenuse is less than or equal to the circle's radius.
-        public bool OverlapsCircle(float radius) => Hypotenuse <= radius;
-
-        public override string ToString() => $"({X}, {Y}) Hyp: {Hypotenuse}";
+        public bool OverlapsCircle(float radius) => _hypotenuse <= radius;
     }
 }
